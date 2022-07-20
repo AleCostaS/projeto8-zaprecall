f9cd6e90d9634e9dd3064b0267eb18c41b792bee
AleCostaS <alexandrec.silva@usp.br>
2022-07-20 19:22:11 -0300

    feito o css e html da meta de zaps

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import ReactDOM from 'react-dom';
 
-import "./CSS/reset.css";
-import "./CSS/style.css";
+import './CSS/reset.css';
+import './CSS/style.css';
+import logo from './img/logo.png' 
 
 ReactDOM.render(
-    <div>
-        sei la 2
-    </div>,
+    <>
+        <div className='meta'>
+            <img src={logo} />
+            <h1>ZapRecall</h1>
+            <input placeholder='Digite sua meta de zaps...'></input>
+            <div className='botão desativado'>Iniciar Recall!</div>
+        </div>
+    </>,
     document.querySelector(".root")
 );
