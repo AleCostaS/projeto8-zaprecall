af2053119b1e27107de63f2565c2368c78719b48
AleCostaS <alexandrec.silva@usp.br>
2022-07-20 18:00:03 -0300

    colocando reset css

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom';
+
+import "./CSS/reset.css";
+import "./CSS/style.css";
+
+ReactDOM.render(
+    <div>
+        sei la 2
+    </div>,
+    document.querySelector(".root")
+);
