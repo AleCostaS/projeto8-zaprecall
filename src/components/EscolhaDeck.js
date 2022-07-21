19c87c1fb4e51a9c6044fea9b486fcd3bd46fe35
AleCostaS <alexandrec.silva@usp.br>
2022-07-20 23:56:55 -0300

    pegando os decks salvos e selecionando eles

--- src/components/EscolhaDeck.js
+++ src/components/EscolhaDeck.js
@@ -1,16 +1,33 @@
 import logo from '../img/logo.png';
 
 export default function EscolhaDeck(props){
+    console.log(props)
+
+    const deckValido = () => {
+        document.querySelector('.botão').classList.remove('desativado')
+        document.querySelector('.botão').classList.add('ativado')
+        document.querySelector('.botão').addEventListener('click', props.mudarTela);
+    };
+
     return (
         <>
             <div className='decks'>
                 <img src={logo} />
                 <h1>ZapRecall</h1>
-                <select>
+                <select onChange={deckValido}>
                     <option value='' disabled selected hidden>Escolha seu deck</option>
+                    {props.decksNames.map((nome, item) => {
+                        return <Opções value={item+1} name={nome}/>;
+                    })}
                 </select>
                 {props.button}
             </div>
         </>
+    );
+}
+
+function Opções(props){
+    return (
+        <option value={props.value}>{props.name}</option>
     );
 }
--- src/index.js
+++ src/index.js
@@ -6,10 +6,40 @@ import EscolhaDeck from './components/EscolhaDeck';
 import './CSS/reset.css';
 import './CSS/style.css';
 
-
-
 function App() {
     const [tela, setTela] = React.useState(0);
+
+    const decks = [
+        {
+            'nome':'React',
+            'pergunta1':{
+                'pergunta':'O que é JSX?', 
+                'resposta':'Uma extensão de linguagem do JavaScript'
+            },
+            'pergunta2':{
+                'pergunta':'O React é __', 
+                'resposta':'uma biblioteca JavaScript para construção de interfaces'
+            },
+            'pergunta3':{
+                'pergunta':'Componentes devem iniciar com __', 
+                'resposta':'letra maiúscula'
+            },
+            'pergunta4':{
+                'pergunta':'Podemos colocar __ dentro do JSX', 'resposta':'expressões'},
+            'pergunta5':{
+                'pergunta':'O ReactDOM nos ajuda __', 
+                'resposta':'interagindo com a DOM para colocar componentes React na mesma'},
+            'pergunta6':{
+                'pergunta':'Usamos o npm para __', 
+                'resposta':'gerenciar os pacotes necessários e suas dependências'},
+            'pergunta7':{
+                'pergunta':'Usamos props para __', 
+                'resposta':'passar diferentes informações para componentes'},
+            'pergunta8':{
+                'pergunta':'Usamos estado (state) para __', 
+                'resposta':'dizer para o React quais informações quando atualizadas devem renderizar a tela novamente'}
+        }
+    ];
 
     if (tela === 0){
         return (
@@ -17,10 +47,21 @@ function App() {
                 <Meta button={<div className='botão desativado'>Iniciar Recall!</div>} mudarTela={() => setTela(tela+1)}/>
             </>
         );
-    } else {
+    } else if (tela === 1){
+        const names = [];
+        for (let i =0; i < decks.length; i++){
+            names.push(decks[i].nome);
+        }
+        console.log(names)
         return (
             <>
-                <EscolhaDeck/>
+                <EscolhaDeck button={<div className='botão desativado'>Iniciar Recall!</div>} mudarTela={() => setTela(tela+1)} decksNames={names}/>
+            </>
+        );
+    }  else {
+        return (
+            <>
+                <div>Em obras</div>
             </>
         );
     }
